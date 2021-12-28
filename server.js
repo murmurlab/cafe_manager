3462bf8dbe8d100dfd3f1c60013b99bf48f12b7b
murmurLAB <0aeonchannel0@gmail.com>
2021-12-28 20:11:02 +0300

    404 PAGE

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 import http from "http"
+import fs from "fs"
+const listener = (req,res)=>{
+	
 
-const listen = (req,res)=>{
-    switch(req.url){
-        case "/":{
-            console.log(1);
-        }
-        default:{
-
-        }
-        }
-            
-        
-    }
+	switch(req.url){
+	case "/":{
+		console.log(1)
+	}break
+	default:{
+		fs.readFile("./client/404.html",(e,d)=>{
+			if(e){
+				console.log("404 page not found")
+			}else{
+				res.write(d)
+				res.end()
+			}
+		})
+	}
+	}
 }
 
-http.createServer(listen)
+http.createServer(listener).listen(80,"::1",()=>{console.log("ok");})
